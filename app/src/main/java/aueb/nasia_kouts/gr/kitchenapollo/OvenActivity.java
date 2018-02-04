498a02d7a0f2f3e07adb48dd0673968a8f6f92df
ChasakisD <chasakisd@gmail.com>
2018-02-04 13:32:33 +0200

    by tony

---
+++ app/src/main/java/aueb/nasia_kouts/gr/kitchenapollo/OvenActivity.java
@@ -0,0 +1,36 @@
+package aueb.nasia_kouts.gr.kitchenapollo;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.triggertrap.seekarc.SeekArc;
+
+public class OvenActivity extends AppCompatActivity {
+
+    SeekArc ovenTemp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oven);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        // ovenTemp.setOnSeekArcChangeListener();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
