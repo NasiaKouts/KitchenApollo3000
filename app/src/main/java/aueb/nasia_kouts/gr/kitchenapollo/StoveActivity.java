6ee114fd121bdfcfc7e4748e68cde92b1b52f171
ChasakisD <chasakisd@gmail.com>
2018-02-05 02:21:37 +0200

    Fixed minor(wtf) bugs in stove activity

--- app/src/main/java/aueb/nasia_kouts/gr/kitchenapollo/StoveActivity.java
+++ app/src/main/java/aueb/nasia_kouts/gr/kitchenapollo/StoveActivity.java
@@ -2,6 +2,7 @@ package aueb.nasia_kouts.gr.kitchenapollo;
 
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
+import android.os.SystemClock;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,7 @@ public class StoveActivity extends AppCompatActivity {
 
     ImageButton leftTopStove, rightTopStove, leftBottomStove, rightBottomStove;
     boolean[] isOpen = new boolean[4];
+    boolean[] isBigOpened = new boolean[2];
 
     TextToSpeech textToSpeechClient;
     int result;
@@ -51,10 +53,10 @@ public class StoveActivity extends AppCompatActivity {
         leftBottomStove = findViewById(R.id.ib_stove_leftbottom);
         rightBottomStove = findViewById(R.id.ib_stove_rightbottom);
 
-        leftTopStove.setOnClickListener(new StoveOnClickListener());
-        rightTopStove.setOnClickListener(new StoveOnClickListener());
-        leftBottomStove.setOnClickListener(new StoveOnClickListener());
-        rightBottomStove.setOnClickListener(new StoveOnClickListener());
+        leftTopStove.setOnClickListener(new StoveOnClickListener(0));
+        rightTopStove.setOnClickListener(new StoveOnClickListener(1));
+        leftBottomStove.setOnClickListener(new StoveOnClickListener(2));
+        rightBottomStove.setOnClickListener(new StoveOnClickListener(3));
 
         stoveControls = new View[4];
         stoveControls[0] = findViewById(R.id.buttons_leftup);
@@ -77,112 +79,198 @@ public class StoveActivity extends AppCompatActivity {
 
     }
 
+    private ImageButton getImageButtonFromPos(int pos){
+        ImageButton imageButton = null;
+        switch (pos){
+            case 0: { imageButton = leftTopStove; break;}
+            case 1: { imageButton = rightTopStove; break;}
+            case 2: { imageButton = leftBottomStove; break;}
+            case 3: { imageButton = rightBottomStove; break;}
+        }
+
+        return imageButton;
+    }
+
+    private String getStringFromStove(int pos, boolean opened, boolean bigOpened){
+        switch (pos){
+            case 0:{
+                return opened ?
+                        getResources().getString(R.string.stove_left_top_opened) :
+                        getResources().getString(R.string.stove_left_top_closed);
+            }
+            case 1:{
+                return opened ? (bigOpened ?
+                        getResources().getString(R.string.stove_right_top_big_opened) :
+                        getResources().getString(R.string.stove_right_top_opened)) :
+                        getResources().getString(R.string.stove_right_top_closed);
+            }
+            case 2:{
+                return opened ? (bigOpened ?
+                        getResources().getString(R.string.stove_left_bottom_big_opened) :
+                        getResources().getString(R.string.stove_left_bottom_opened)) :
+                        getResources().getString(R.string.stove_left_bottom_closed);
+            }
+            default:{
+                return opened ?
+                        getResources().getString(R.string.stove_right_bottom_opened) :
+                        getResources().getString(R.string.stove_right_bottom_closed);
+            }
+        }
+    }
+
+    private void openStove(int pos){
+        LayerDrawable layer = (LayerDrawable) (getImageButtonFromPos(pos)).getDrawable();
+
+        int firstItem = 0;
+        if(pos == 1 || pos == 2) firstItem = 1;
+
+        GradientDrawable gradient = (GradientDrawable)layer.findDrawableByLayerId(layer.getId(firstItem));
+        gradient.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        gradient.setGradientRadius(150);
+        gradient.setColors(new int[] {getResources().getColor(android.R.color.holo_red_light), getResources().getColor(android.R.color.holo_red_dark)});
+        isOpen[pos] = true;
+
+        String msg = getStringFromStove(pos, isOpen[pos], false);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void openBigStove(int pos){
+        if(pos == 0 || pos == 3) return;
+
+        LayerDrawable layer = (LayerDrawable) (getImageButtonFromPos(pos)).getDrawable();
+
+        GradientDrawable gradient = (GradientDrawable)layer.findDrawableByLayerId(layer.getId(0));
+        gradient.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        gradient.setGradientRadius(150);
+        gradient.setColors(new int[] {getResources().getColor(android.R.color.holo_red_light), getResources().getColor(android.R.color.holo_red_dark)});
+        isOpen[pos] = true;
+        isBigOpened[pos-1] = true;
+
+        String msg = getStringFromStove(pos, isOpen[pos], true);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void closeStove(int pos){
+        LayerDrawable layer = (LayerDrawable) (getImageButtonFromPos(pos)).getDrawable();
+
+        int firstItem = 0;
+        if(pos == 1 || pos == 2) firstItem = 1;
+
+        GradientDrawable gradient = (GradientDrawable)layer.findDrawableByLayerId(layer.getId(firstItem));
+        gradient.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        gradient.setGradientRadius(150);
+        gradient.setColors(new int[] {getResources().getColor(android.R.color.transparent), getResources().getColor(android.R.color.transparent)});
+
+        if(pos == 1 || pos == 2){
+            GradientDrawable gradient2 = (GradientDrawable)layer.findDrawableByLayerId(layer.getId(0));
+            gradient2.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+            gradient2.setGradientRadius(150);
+            gradient2.setColors(new int[] {getResources().getColor(android.R.color.transparent), getResources().getColor(android.R.color.transparent)});
+        }
+
+        isOpen[pos] = false;
+        if(pos == 1 || pos == 2){
+            isBigOpened[pos-1] = false;
+        }
+
+        String msg = getStringFromStove(pos, isOpen[pos], false);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
     private class StoveOnClickListener extends OnDoubleClickListener{
+        private int pos;
+
+        private StoveOnClickListener(int pos){
+            this.pos = pos;
+        }
+
         @Override
         public void onClick(View v) {
             if(isDoubleClick()) {
                 onDoubleClick(v);
             }else{
-                LayerDrawable twoStove = (LayerDrawable) ((ImageButton)v).getDrawable();
-                int pos = -1;
-                switch (twoStove.getId(0)){
-                    case R.id.stove1:{
-                        pos = 0;
-                        break;
+                if(pos == 0 || pos == 3){
+                    if(isOpen[pos]){
+                        closeStove(pos);
                     }
-                    case R.id.outerStove1: {
-                        pos = 1;
-                        break;
+                    else{
+                        openStove(pos);
                     }
-                    case R.id.outerStove2:{
-                        pos = 2;
-                        break;
-                    }
-                    case R.id.stove2:{
-                        pos = 3;
-                        break;
-                    }
-
+                    return;
                 }
-                System.out.println(pos);
 
-                int firstItem = 0;
-                if(pos == 1 || pos == 2) firstItem = 1;
+                final boolean isBigOpenBefore = isBigOpened[pos-1];
+                final boolean isOpenedBefore = isOpen[pos];
+                System.out.println("Before Big: " + isBigOpenBefore);
+                System.out.println("Before Open: " + isOpenedBefore);
 
-                if(isOpen[pos]){
-                    GradientDrawable gradient = (GradientDrawable)twoStove.findDrawableByLayerId(twoStove.getId(firstItem));
-                    gradient.setGradientType(GradientDrawable.RADIAL_GRADIENT);
-                    gradient.setGradientRadius(150);
-                    gradient.setColors(new int[] {getResources().getColor(android.R.color.transparent), getResources().getColor(android.R.color.transparent)});
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        SystemClock.sleep(200);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                System.out.println("After Big: " + isBigOpened[pos-1]);
+                                System.out.println("After Open: " + isOpen[pos]);
+                                if(!isBigOpenBefore && isBigOpened[pos-1]){
+                                    System.out.println("Going to abort");
+                                    return;
+                                }
 
-                    if(pos == 1 || pos == 2){
-                        GradientDrawable gradient2 = (GradientDrawable)twoStove.findDrawableByLayerId(twoStove.getId(0));
-                        gradient2.setGradientType(GradientDrawable.RADIAL_GRADIENT);
-                        gradient2.setGradientRadius(150);
-                        gradient2.setColors(new int[] {getResources().getColor(android.R.color.transparent), getResources().getColor(android.R.color.transparent)});
+                                if(isOpen[pos]){
+                                    System.out.println("Going to close");
+                                    closeStove(pos);
+                                }
+                                else{
+                                    System.out.println("Going to open");
+                                    openStove(pos);
+                                }
+                            }
+                        });
                     }
-
-                    isOpen[pos] = false;
-                }
-                else{
-                    GradientDrawable gradient = (GradientDrawable)twoStove.findDrawableByLayerId(twoStove.getId(firstItem));
-                    gradient.setGradientType(GradientDrawable.RADIAL_GRADIENT);
-                    gradient.setGradientRadius(150);
-                    gradient.setColors(new int[] {getResources().getColor(android.R.color.holo_red_light), getResources().getColor(android.R.color.holo_red_dark)});
-                    isOpen[pos] = true;
-                }
+                }).start();
             }
+
             updateTime();
         }
 
         @Override
         public void onDoubleClick(View view) {
-            LayerDrawable twoStove = (LayerDrawable) ((ImageButton)view).getDrawable();
-            int pos = 0;
-            switch (twoStove.getId(0)){
-                case R.id.stove1:{
-                    return;
-                }
-                case R.id.outerStove1: {
-                    pos = 1;
-                    break;
-                }
-                case R.id.outerStove2:{
-                    pos = 2;
-                    break;
-                }
-                case R.id.stove2:{
-                    return;
-                }
-            }
-            GradientDrawable gradient = (GradientDrawable)twoStove.findDrawableByLayerId(twoStove.getId(0));
-            gradient.setGradientType(GradientDrawable.RADIAL_GRADIENT);
-            gradient.setGradientRadius(150);
-            gradient.setColors(new int[] {getResources().getColor(android.R.color.holo_red_light), getResources().getColor(android.R.color.holo_red_dark)});
-            isOpen[pos] = true;
+            openBigStove(pos);
         }
     }
 
     private class StoveControlsOnClickListener implements View.OnClickListener{
+        private int stoveId;
         private boolean isAddition;
         private TextView label;
 
-        public StoveControlsOnClickListener(int id, boolean isAddition, TextView label){
+        private StoveControlsOnClickListener(int stoveId, boolean isAddition, TextView label){
+            this.stoveId = stoveId;
             this.isAddition = isAddition;
             this.label = label;
         }
 
         @Override
         public void onClick(View view) {
-            int num = Integer.parseInt(label.getText().toString());
+            int numBefore = Integer.parseInt(label.getText().toString());
+            int newNum = numBefore;
             if(isAddition){
-                if(num < 9){
-                    label.setText("" + ++num);
+                if(numBefore < 9){
+                    label.setText("" + ++newNum);
                 }
             }else{
-                if(num > 0){
-                    label.setText("" + --num);
+                if(numBefore > 0){
+                    label.setText("" + --newNum);
                 }
+            }
+
+            if(newNum == 0){
+                closeStove(stoveId);
+            }
+            else if(newNum > 0 && numBefore == 0){
+                openStove(stoveId);
             }
         }
     }
